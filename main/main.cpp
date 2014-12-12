530c2b1643cc95bc0ad9e176302eb06fe09b6137
Jonas Kümmerlin <rgcjonas@gmail.com>
2014-12-12 17:30:54 +0100

    Don't force the font for the whole application
    but still keep our DejaVu ttf around to use it in a few locations.

--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,8 @@
 #include <QFont>
 #include <QFontDatabase>
 
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -11,13 +13,8 @@ int main(int argc, char *argv[])
     // setup font substitution, since windows fonts miss some glyphs
     QFontDatabase::addApplicationFont(":/DejaVuSans.ttf");
 
-    //Doesn't work in WinXP with Qt5.1 :(
-    //QFont::insertSubstitution("Tahoma", "DejaVu Sans");
-
     // create and show the main window
     MainWindow w;
-
-    a.setFont(QFont("DejaVu Sans", w.font().pointSize()));
 
     w.show();
 
