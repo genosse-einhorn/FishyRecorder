46f948660498af2bd1b62077596eb5b69562656c
Jonas Kümmerlin <rgcjonas@gmail.com>
2015-02-22 18:23:07 +0100

    Added new screen viewer based on my win32-screen-view project
    Remaining issues:
     * The x86 DLL is included in every build, even on Linux
     * Window exclusion is impossible
     * WinXP/Vista compatibility is unlikely to happen
     * The Win7 implementation is a giant hack.

--- presentation/screenviewcontrol.cpp
+++ presentation/screenviewcontrol.cpp
@@ -8,51 +8,102 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <QTimer>
+#include <QDir>
+#include <QTemporaryFile>
+#include <algorithm>
 
 namespace {
+class screenview_t {
+    typedef void (__cdecl *SV_LogHandler_t)(const char *message, void *userdata);
 
-typedef struct tagMAGTRANSFORM {
-  float v[3][3];
-} MAGTRANSFORM, *PMAGTRANSFORM;
+    void (__cdecl *SV_SetLogHandler)(SV_LogHandler_t handler, void *userdata);
+    HWND (__cdecl *SV_CreateView)(HWND parent, int x, int y, int w, int h);
+    void (__cdecl *SV_ChangeScreen)(HWND view, int x, int y, int w, int h);
 
-enum FilterMode
-{
-    MW_FILTERMODE_EXCLUDE = 0,
-    MW_FILTERMODE_INCLUDE = 1
-};
+    QFile dll;
+    QLibrary lib;
 
-#define MS_SHOWMAGNIFIEDCURSOR 0x0001L
+public:
+    screenview_t() : dll(QDir::temp().absoluteFilePath("screenview-x86.dll"))
+    {
+        // Extract DLL
+        if (!dll.open(QFile::WriteOnly)) {
+            qWarning() << "Failed to open dll file:" << dll.errorString();
+            return;
+        }
 
-BOOL (WINAPI *MagInitialize)() = nullptr;
-BOOL (WINAPI *MagUninitialize)();
-BOOL (WINAPI *MagSetWindowSource)(HWND hwnd, RECT rect);
-BOOL (WINAPI *MagSetWindowTransform)(HWND hwnd, PMAGTRANSFORM pTransform);
-BOOL (WINAPI *MagSetWindowFilterList)(HWND hwnd, DWORD dwFilterMode, int count, HWND *pHWND);
+        QFile original(":/screenview-x86.dll");
+        if (!original.open(QIODevice::ReadOnly)) {
+            qWarning() << "Failed to open original dll file:" << original.errorString();
+            return;
+        }
 
-bool initializeMagnificationApi() {
-    if (MagInitialize)
-        return true;
+        while (!original.atEnd()) {
+            char buf[1024];
+            qint64 read = original.read(buf, sizeof(buf));
+            dll.write(buf, read);
+        }
 
-    QLibrary lib("Magnification.dll");
+        dll.close();
 
-    if (!lib.load()) {
-        qWarning() << "Magnification.dll is missing :(";
-        return false;
-    }
+        // Load it
+        lib.setFileName(dll.fileName());
+        if (!lib.load()) {
+            qWarning() << "Failed to load library" << lib.errorString();
+            return;
+        }
 
-#define RESOLVE(func) func = (decltype(func)) lib.resolve(#func)
-
-    RESOLVE(MagInitialize);
-    RESOLVE(MagUninitialize);
-    RESOLVE(MagSetWindowSource);
-    RESOLVE(MagSetWindowTransform);
-    RESOLVE(MagSetWindowFilterList);
-
+        // Resolve our stuff
+#define RESOLVE(name) this->name = (decltype(this->name))lib.resolve(#name)
+        RESOLVE(SV_SetLogHandler);
+        RESOLVE(SV_CreateView);
+        RESOLVE(SV_ChangeScreen);
 #undef RESOLVE
 
-    return MagInitialize();
-}
+        if (!SV_SetLogHandler || !SV_CreateView || !SV_ChangeScreen) {
+            qWarning() << "Failed to load functions from the screenview dll";
+            return;
+        }
 
+        SV_SetLogHandler(&OurLogHandler, nullptr);
+    }
+
+    ~screenview_t()
+    {
+        // This is not as neat as it could be because the file stays around for an indefinite time,
+        // but it's the best we can get without doing complicated work.
+        MoveFileEx((const wchar_t*)dll.fileName().utf16(), nullptr, MOVEFILE_DELAY_UNTIL_REBOOT);
+    }
+
+    HWND createView(HWND parent, int x, int y, int w, int h)
+    {
+        if (!SV_CreateView) {
+            qWarning() << "screenview dll not present :(";
+            return 0;
+        }
+
+        return SV_CreateView(parent, x, y, w, h);
+    }
+
+    void changeScreen(HWND view, int x, int y, int w, int h)
+    {
+        if (!SV_ChangeScreen) {
+            qWarning() << "screenview dll not present :(";
+            return;
+        }
+
+        SV_ChangeScreen(view, x, y, w, h);
+    }
+
+private:
+    static __cdecl void OurLogHandler(const char *message, void*)
+    {
+        qDebug() << "SCREENVIEW:" << message;
+    }
+
+};
+
+    static screenview_t screenview;
 }
 
 namespace Presentation {
@@ -60,69 +111,11 @@ namespace Presentation {
 ScreenViewControl::ScreenViewControl(QWidget *parent) :
     QWinHost(parent)
 {
-    if (!initializeMagnificationApi()) {
-        qWarning() << "Failed to initialize magnification api :(";
-
-        QGridLayout *l = new QGridLayout(this);
-        l->addWidget(new QLabel("Magnification API not ready", this));
-
-        this->setLayout(l);
-
-        return;
-    } else {
-        this->m_magApiReady = true;
-    }
-
-    m_resizeTimer = new QTimer(this);
-    QObject::connect(m_resizeTimer, &QTimer::timeout, this, &ScreenViewControl::doResize);
-
-    m_invalidateTimer = new QTimer(this);
-    QObject::connect(m_invalidateTimer, &QTimer::timeout, this, &ScreenViewControl::invalidateScreen);
-    m_invalidateTimer->setInterval(50);
-
-    // create a window class for the separate host window
-    WNDCLASSEX wcex;
-    memset(&wcex, 0, sizeof wcex);
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = DefWindowProc;
-    wcex.hInstance      = GetModuleHandle(NULL);
-    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(1 + COLOR_BTNFACE);
-    wcex.lpszClassName  = L"MagnifierWindow";
-
-    RegisterClassEx(&wcex); // ignore error
-
-    // create magnification window
-    m_hostWindow = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_LAYERED | WS_EX_TRANSPARENT,
-                                  L"MagnifierWindow", L"Magnifier Window",
-                                  WS_CLIPCHILDREN | WS_POPUP,
-                                  0, 0, 300, 300, NULL, NULL, qWinAppInst(), NULL);
-    if (!m_hostWindow)
-    {
-        qWarning() << "Error CreateWindowEx (host window)" << GetLastError();
-    }
-
-    // Make the window opaque.
-    SetLayeredWindowAttributes(m_hostWindow, 0, 255, LWA_ALPHA);
-
-    // Create a magnifier control that fills the client area.
-    RECT magWindowRect;
-    GetClientRect(m_hostWindow, &magWindowRect);
-    m_magnifier = CreateWindow(L"Magnifier", TEXT("MagnifierWindow"),
-                               WS_CHILD | MS_SHOWMAGNIFIEDCURSOR | WS_VISIBLE,
-                               magWindowRect.left, magWindowRect.top, magWindowRect.right, magWindowRect.bottom,
-                               m_hostWindow, NULL, qWinAppInst(), NULL );
-    if (!m_magnifier)
-    {
-        qWarning() << "Error CreateWindow (Magnifier control)" << GetLastError();
-    }
 }
 
 ScreenViewControl::~ScreenViewControl()
 {
-    if (m_magApiReady)
-        MagUninitialize();
+
 }
 
 void ScreenViewControl::screenUpdated(const QRect &rect)
@@ -131,118 +124,60 @@ void ScreenViewControl::screenUpdated(const QRect &rect)
 
     qDebug() << "Screen has been set to" << rect;
 
-    if (!m_magApiReady || !m_screen.width())
-        return;
+    this->updateGeometry();
 
-    RECT r { rect.left(), rect.top(), rect.right() + 1, rect.bottom() + 1 };
-    if (!MagSetWindowSource(m_magnifier, r))
-        qWarning() << "Failed: MagSetWindowSource";
+    if (window())
+        screenview.changeScreen(window(), rect.x(), rect.y(), rect.width(), rect.height());
 }
 
-void ScreenViewControl::setExcludedWindow(HWND win)
+void ScreenViewControl::setExcludedWindow(HWND)
 {
-    m_excludedWindow = win;
-
-    if (!m_magApiReady)
-        return;
-
-    qDebug() << "Setting excluded window" << win;
-    HWND excludedWindows[] = { win, (HWND)this->topLevelWidget()->winId() };
-    if (!MagSetWindowFilterList(m_magnifier, MW_FILTERMODE_EXCLUDE, 2, excludedWindows))
-        qWarning() << "MagSetWindowFilterList failed!" << GetLastError();
+    qWarning() << "Excluding not supported :(";
 }
 
-void ScreenViewControl::doResize()
+QSize ScreenViewControl::getViewWindowSize()
 {
-    m_resizeTimer->stop();
-    m_invalidateTimer->start();
+    if (!m_screen.width() | !m_screen.height())
+        return QSize(width(), height());
 
-    if (!m_magApiReady || !m_screen.width())
-        return;
+    double available_width = static_cast<double>(width());
+    double available_height = static_cast<double>(height());
+    double screen_width = static_cast<double>(m_screen.width());
+    double screen_height = static_cast<double>(m_screen.height());
 
-    // calculate and set a new magnification factor
-    float factorX = ((float)width()/m_screen.width());
-    float factorY = ((float)height()/m_screen.height());
+    double factor_x = available_width/screen_width;
+    double factor_y = available_height/screen_height;
 
-    float factor = qMin(factorX, factorY);
+    double factor = std::min(factor_x, factor_y);
 
-    MAGTRANSFORM transform;
-    memset(&transform, 0, sizeof(MAGTRANSFORM));
-    transform.v[0][0] = factor;
-    transform.v[1][1] = factor;
-    transform.v[2][2] = 1.0f;
-
-    qDebug() << "Setting transformation factor" << factor;
-
-    MEASURE_TIME(MagSetWindowTransform(m_magnifier, &transform));
-    RECT r = { m_screen.left(), m_screen.top(), m_screen.right()+1, m_screen.bottom()+1 };
-    MEASURE_TIME(MagSetWindowSource(m_magnifier, r));
-
-    // resize hosting window
-    auto res = SetWindowPos(m_hostWindow, NULL, 0, 0, width(), height(), SWP_NOACTIVATE | SWP_SHOWWINDOW | SWP_NOZORDER);
-    if (!res)
-        qWarning() << "SetWindowPos for host window failed:" << GetLastError();
-
-    // resize the magnifier control inside the host window
-    RECT cr;
-    ::GetClientRect(m_hostWindow, &cr);
-    MEASURE_TIME(SetWindowPos(m_magnifier, NULL, cr.left, cr.top, cr.right-cr.left, cr.bottom-cr.top, SWP_NOACTIVATE | SWP_NOZORDER | SWP_SHOWWINDOW));
-
-
-}
-
-void ScreenViewControl::invalidateScreen()
-{
-    ::InvalidateRect(m_magnifier, nullptr, true);
+    return QSize(static_cast<int>(screen_width * factor), static_cast<int>(screen_height * factor));
 }
 
 } // namespace Presentation
 
-
 HWND Presentation::ScreenViewControl::createWindow(HWND parent, HINSTANCE instance)
 {
-    Q_UNUSED(parent);
     Q_UNUSED(instance);
 
-    return m_hostWindow;
+    return screenview.createView(parent, m_screen.x(), m_screen.y(), m_screen.width(), m_screen.height());
 }
 
-
-void Presentation::ScreenViewControl::resizeEvent(QResizeEvent *ev)
+void Presentation::ScreenViewControl::resizeEvent(QResizeEvent *e)
 {
-    // skip the window host, but chain up to qwidget right away
-    QWidget::resizeEvent(ev);
+    QWidget::resizeEvent(e);
 
-    m_invalidateTimer->stop();
-    m_resizeTimer->stop();
-    m_resizeTimer->start(1000);
-
-    ::ShowWindow(m_hostWindow, SW_HIDE);
+    if (window()) {
+        QSize sz = getViewWindowSize();
+        SetWindowPos(window(), HWND_TOP, (width()-sz.width())/2, (height()-sz.height())/2, sz.width(), sz.height(), 0);
+    }
 }
 
 void Presentation::ScreenViewControl::showEvent(QShowEvent *e)
 {
     QWidget::showEvent(e);
 
-    m_invalidateTimer->start();
-    ::ShowWindow(m_hostWindow, SW_SHOWNOACTIVATE);
-}
-
-void Presentation::ScreenViewControl::hideEvent(QHideEvent *e)
-{
-    QWidget::hideEvent(e);
-
-    m_invalidateTimer->stop();
-    ::ShowWindow(m_hostWindow, SW_HIDE);
-}
-
-void Presentation::ScreenViewControl::focusInEvent(QFocusEvent *ev)
-{
-    QWidget::focusInEvent(ev);
-}
-
-
-bool Presentation::ScreenViewControl::nativeEvent(const QByteArray &eventType, void *message, long *result)
-{
-    return QWidget::nativeEvent(eventType, message, result);
+    if (window()) {
+        QSize sz = getViewWindowSize();
+        SetWindowPos(window(), HWND_TOP, (width()-sz.width())/2, (height()-sz.height())/2, sz.width(), sz.height(), SWP_SHOWWINDOW);
+    }
 }
--- presentation/screenviewcontrol.h
+++ presentation/screenviewcontrol.h
@@ -20,19 +20,10 @@ public slots:
     void screenUpdated(const QRect& rect);
     void setExcludedWindow(HWND win);
 
-private slots:
-    void doResize();
-    void invalidateScreen();
-
 private:
     QRect   m_screen          = QRect(0, 0, 0, 0);
-    bool    m_magApiReady     = false;
-    QTimer *m_resizeTimer     = nullptr;
-    QTimer *m_invalidateTimer = nullptr;
-    HWND    m_excludedWindow  = 0;
 
-    HWND    m_hostWindow = NULL;
-    HWND    m_magnifier  = NULL;
+    QSize getViewWindowSize();
 
     // QWinHost interface
 protected:
@@ -42,9 +33,6 @@ protected:
 protected:
     virtual void resizeEvent(QResizeEvent *);
     virtual void showEvent(QShowEvent *);
-    virtual void hideEvent(QHideEvent *);
-    virtual void focusInEvent(QFocusEvent *);
-    virtual bool nativeEvent(const QByteArray &eventType, void *message, long *result);
 };
 
 } // namespace Presentation
